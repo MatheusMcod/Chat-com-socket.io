a52272055ac952bf70969e9d3b42451a3585db1c
Matheus Moreira Costa <90786402+MatheusMcod@users.noreply.github.com>
2024-02-29 09:52:04 -0300

    style

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 let express = require("express");
+const { Socket } = require("socket.io");
 let app = express();
 let http = require("http").createServer(app);
 let io = require("socket.io")(http);
+
+io.on("connection", (socket) => {
+    socket.on("msg", (data) => {
+        io.emit("showmsg", data);
+    });
+});
 
 app.set("view engine", "ejs");
 
